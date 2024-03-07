111fb5232bcbd000ffe66969b8e8c589ba83697f
MrAndroPC <sared3420@gmail.com>
2024-03-07 16:19:18 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VDC_WPF_T.Model;
+
+
+namespace VDC_WPF_T
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        internal ObservableCollection<Pet> Pets { get; set; }
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+
+
+            Pets = new ObservableCollection<Pet>
+        {
+            new Pet { Id = 1, Name = "Bob", Contacts = "123-456-7890", Type = "Dog", Sex = "Male", Age = 3, Breed = "Labrador", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 1, Name = "Bob", Contacts = "123-456-7890", Type = "Dog", Sex = "Male", Age = 3, Breed = "Labrador", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 1, Name = "Bob", Contacts = "123-456-7890", Type = "Dog", Sex = "Male", Age = 3, Breed = "Labrador", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 2, Name = "Jim", Contacts = "456-789-0123", Type = "Cat", Sex = "Male", Age = 5, Breed = "Siamese", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 1, Name = "Bob", Contacts = "123-456-7890", Type = "Dog", Sex = "Male", Age = 3, Breed = "Labrador", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 1, Name = "Bob", Contacts = "123-456-7890", Type = "Dog", Sex = "Male", Age = 3, Breed = "Labrador", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 2, Name = "Jim", Contacts = "456-789-0123", Type = "Cat", Sex = "Male", Age = 5, Breed = "Siamese", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 1, Name = "Bob", Contacts = "123-456-7890", Type = "Dog", Sex = "Male", Age = 3, Breed = "Labrador", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 1, Name = "Bob", Contacts = "123-456-7890", Type = "Dog", Sex = "Male", Age = 3, Breed = "Labrador", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+            new Pet { Id = 2, Name = "Jim", Contacts = "456-789-0123", Type = "Cat", Sex = "Male", Age = 5, Breed = "Siamese", PicSource = @"C:\Users\sared\Downloads\pepefrg-44.gif\" },
+        };
+
+            // Set the ItemsSource of the ListBox to the Pets collection
+            TestList.ItemsSource = Pets;
+
+
+
+        }
+        private void gif_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            ((MediaElement)sender).Position = new TimeSpan(0, 0, 1);
+            ((MediaElement)sender).Play();
+        }
+
+
+        private void Button_Click()
+        {
+
+        }
+    }
+
+}
---
+++ Model/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDC_WPF_T.Model
+{
+        public class Pet
+        {
+            public int Id { get; set; } 
+            public string Name { get; set; }
+            public string Contacts { get; set; }
+            public string Type { get; set; }
+            public string Sex { get; set; }
+            public int Age { get; set; }
+            public string Breed { get; set; }
+            public string PicSource { get; set; }
+
+        }
+}
